0b90aa5aa365b1b96f980ea67645b8b0197b737c
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2024-12-17 17:10:09 -0300

    ForEach

--- arrays/ForEach.java
+++ arrays/ForEach.java
@@ -4,6 +4,18 @@ public class ForEach {
 
     public static void main(String[] args) {
 
+        double[] notas = {9.9, 8.7, 7.2, 9.4};
 
+        // For normal
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println(notas[i] + " ");
+        };
+
+        System.out.println();
+
+        // For each
+        for (double nota: notas) {
+            System.out.println(nota + " ");
+        };
     }
 }
