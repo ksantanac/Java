486285022ee6c0408cb03797f8aad246d02cd55a
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-01-07 21:52:47 -0300

    Classe Abstrata.

---
+++ oo/abstrato/Cachorro.java
@@ -0,0 +1,14 @@
+package oo.abstrato;
+
+public class Cachorro extends Mamifero{
+
+//    @Override
+//    public String mover() {
+//        return "Patas";
+//    }
+
+    @Override
+    public String mamar() {
+        return "Leite";
+    }
+}
