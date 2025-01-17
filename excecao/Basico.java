60998b8eae45c35163f3ad38b5d3d20ff2f754c0
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-01-16 21:14:30 -0300

    Iniciando a seção de tratamento de erros.

---
+++ excecao/Basico.java
@@ -0,0 +1,29 @@
+package excecao;
+
+public class Basico {
+
+    public static void main(String[] args) {
+
+        Aluno a1 = null;
+        try {
+            imprimirNomeDoAluno(a1);
+        } catch (Exception excecao) {
+            System.out.println("Erro ao imprimir nome do usuário.");
+        }
+
+
+        try {
+            System.out.println(7 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        System.out.println("Fim :)");
+        
+    }
+
+    public static void imprimirNomeDoAluno(Aluno aluno) {
+        System.out.println(aluno.nome);
+    }
+
+}
