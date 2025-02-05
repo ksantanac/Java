ac93488dfef2d426694736a5cde6a9c0a976a31c
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-02-05 17:30:13 -0300

    Padrão observer com swing.

---
+++ swing/Observador.java
@@ -0,0 +1,31 @@
+package swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Observador {
+
+    public static void main(String[] args) {
+
+        JFrame janela = new JFrame("Observador");
+        janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        janela.setSize(600, 200);
+        janela.setLayout(new FlowLayout());
+        janela.setLocationRelativeTo(null); // null -> centralizar na tela do pc
+
+        JButton botao = new JButton("Clicar!");
+        janela.add(botao);
+
+        botao.addActionListener(
+                e -> {
+                    System.out.println("Evento ocorreu!");
+                }
+        );
+        
+        janela.setVisible(true);
+
+    }
+
+}
